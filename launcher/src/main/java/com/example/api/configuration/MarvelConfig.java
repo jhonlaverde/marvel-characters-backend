5317a6988570a5bb5bae1bf1ffaebe52fcc74f14
John Rincon <jhon.laverde@globant.com>
2023-07-01 23:37:23 -0500

    add cors configuration

--- launcher/src/main/java/com/example/api/configuration/MarvelConfig.java
+++ launcher/src/main/java/com/example/api/configuration/MarvelConfig.java
@@ -12,6 +12,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MarvelConfig {
@@ -38,5 +40,15 @@ public class MarvelConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
     }
 }
